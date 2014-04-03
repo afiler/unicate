30982dbdee91a1aad312005ca6ec545257b2013b
Andrew Filer <afiler@gmail.com>
2014-04-02 18:31:04 -0700

    Added @FakeUnicode's region flag things, auto-updating

--- unicate.js
+++ unicate.js
@@ -28,18 +28,61 @@ formats = {
     [/ /g, chr(0x3000)]
   ],
 
-  bold: [0x1d400, 0x1d7ce],
+  //bold: [0x1d400, 0x1d7ce],
+  bold: [
+    [/[A-Z]/g, 0x1d400-0x41],
+    [/[a-z]/g, 0x1d41a-0x61],
+    [/[1-9]/g, 0x1d7ed-0x31],
+    [/0/g, 0x1d7ed-0x31]
+  ],
+
   //italic: 0x1d434,
   italic: [
     ['h', 0x210E],
     [/[A-Z]/g, 0x1d434-0x41],
     [/[a-z]/g, 0x1d434+26-0x61]
   ],
-  bold_italic: [0x1d468, 0x1d7ce],
-  sans_serif: [0x1d5a0, 0x1d7e2],
-  sans_serif_bold: [0x1d5d4, 0x1d7ec],
-  sans_serif_italic: 0x1d608,
-  sans_serif_bold_italic: [0x1d63c, 0x1d7ec],
+  
+  //bold_italic: [0x1d468, 0x1d7ce],
+  bold_italic: [
+    [/[A-Z]/g, 0x1d468-0x41],
+    [/[a-z]/g, 0x1d482-0x61],
+    [/[1-9]/g, 0x1d7ed-0x31],
+    [/0/g, 0x1d7ed-0x31]
+  ],
+  
+  //sans_serif: [0x1d5a0, 0x1d7e2],
+  sans_serif: [
+    [/[A-Z]/g, 0x1d5a0-0x41],
+    [/[a-z]/g, 0x1d5ba-0x61],
+    [/[1-9]/g, 0x1d7e3-0x31],
+    [/0/g, 0x1d7e3-0x31],
+  ],
+    
+  //sans_serif_bold: [0x1d5d4, 0x1d7ec],
+  sans_serif_bold: [
+    [/[A-Z]/g, 0x1d5d4-0x41],
+    [/[a-z]/g, 0x1d5ee-0x61],
+    [/[1-9]/g, 0x1d7ed-0x31],
+    [/0/g, 0x1d7ed-0x31],
+  ],
+  
+  //sans_serif_italic: 0x1d608,
+  sans_serif_italic: [
+    [/[A-Z]/g, 0x1d608-0x41],
+    [/[a-z]/g, 0x1d622-0x61],
+    [/[1-9]/g, 0x1d7e3-0x31],
+    [/0/g, 0x1d7e3-0x31],
+  ],
+  
+  //sans_serif_bold_italic: [0x1d63c, 0x1d7ec],
+  sans_serif_bold_italic: [
+    [/[A-Z]/g, 0x1d63c-0x41],
+    [/[a-z]/g, 0x1d656-0x61],
+    [/[1-9]/g, 0x1d7ed-0x31],
+    [/0/g, 0x1d7ed-0x31],
+  ],
+  
   monospace: [0x1d670, 0x1d7f6],
   
   //script: 0x1d49c,
@@ -87,15 +130,57 @@ formats = {
   ſubſtitute_ſes: function(text) {
     return text.replace(/s(?=[a-z])/g, 'ſ');
   },
+  
+  region_tiles: function(text) {
+    var sub = [
+      //symbols
+      ['!!','\u203c'],  ['!?','\u2049'],  ['?','\u2753'],  ['!','\u2757'],
+      //misc letter groups
+      ['ab','\uD83C\uDD8E'],  ['wc','\uD83C\uDD8F'],  ['dj','\uD83C\uDD90'],  ['cl','\uD83C\uDD91'],
+      ['cool','\uD83C\uDD92'],  ['free','\uD83C\uDD93'],  ['new','\uD83C\uDD95'],  ['ng','\uD83C\uDD96'],
+      ['ok','\uD83C\uDD97'],  ['sos','\uD83C\uDD98'],  ['up','\uD83C\uDD99'],  ['vs','\uD83C\uDD9A'],
+      ['wc','\uD83D\uDEBE'],  ['TM','\u2122'],
+      ['back','\uD83D\uDD19'],  ['end','\uD83D\uDD1A'],  ['soon','\uD83D\uDD1C'],
+      ['on','\uD83D\uDD1B'],  ['top','\uD83D\uDD1D'],
+      //prevent flags, needs editing if they add more!
+      ['CN','C\u00ADN'],  ['DE','D\u00ADE'],  ['ES','E\u00ADS'],  ['FR','F\u00ADR'],
+      ['GB','G\u00ADB'],  ['IT','I\u00ADT'],  ['JP','J\u00ADP'],  ['KR','K\u00ADR'],
+      ['RU','R\u00ADU'],  ['US','U\u00ADS'],
+      //letters
+      ['A','\uD83C\uDDE6'],  ['B','\uD83C\uDDE7'],  ['C','\uD83C\uDDE8'],  ['D','\uD83C\uDD94'],
+      ['E','\uD83C\uDDEA'],  ['F','\uD83C\uDDEB'],  ['G','\uD83C\uDDEC'],  ['H','\uD83C\uDDED'],
+      ['I','\uD83C\uDDEE'],  ['J','\uD83C\uDDEF'],  ['K','\uD83C\uDDF0'],  ['L','\uD83C\uDDF1'],
+      ['M','\uD83C\uDDF2'],  ['N','\uD83C\uDDF3'],  ['O','\uD83C\uDDF4'],  ['P','\uD83C\uDDF5'],
+      ['Q','\uD83C\uDDF6'],  ['R','\uD83C\uDDF7'],  ['S','\uD83C\uDDF8'],  ['T','\uD83C\uDDF9'],
+      ['U','\uD83C\uDDFA'],  ['V','\uD83C\uDDFB'],  ['W','\uD83C\uDDFC'],  ['X','\uD83C\uDDFD'],
+      ['Y','\uD83C\uDDFE'],  ['Z','\uD83C\uDDFF']
+    ];
+
+    for(var i=0;i<sub.length;i++) {
+      var restr = sub[i][0].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+      var re = new RegExp(restr, 'ig');
+      text = text.replace(re, sub[i][1]);
+    }
+    return text;
+  }
 }
 
-function $(v) {
-  return document.getElementById(v)
+function pick() {
+  button = $(this)
+  console.log('pick', button,  button.data('ruleset-name'))
+  window.ruleset_name = button.data('ruleset-name')
+  $('div.box button').removeClass('selected')
+  button.addClass('selected')
+  update()
 }
 
-function go(ruleset_name) {
-  ruleset = formats[ruleset_name]
-  text = $('in-text').value
+function update() {
+  if (!window.ruleset_name) return
+  $('#out-text').val(translate($('#in-text').val()))
+}
+
+function translate(text) {
+  ruleset = formats[window.ruleset_name]
   if (typeof(ruleset) == 'function') {
     text = ruleset(text)
   } else if (typeof(ruleset) == 'number') {
@@ -105,7 +190,7 @@ function go(ruleset_name) {
   } else {
     text = format(ruleset, text)
   }
-  $('out-text').value = text
+  return text
 }
 
 function format(rules, text) {
@@ -167,6 +252,12 @@ function fixedFromCharCode (codePt) {
         return String.fromCharCode(codePt);  
     }  
 }
+
+$(document).ready(function() {
+  $('div.box button').on('click', function() { console.log($(this).data('ruleset-name'))})
+  $('div.box button').on('click', pick)
+  if (!window.ruleset_name) $($('div.box button')[0]).trigger('click')
+})
 
 
 //}
